f4713b527d1cda1e0e58d3a21699761f014fd86a
chisjulius <69173002+chisjulius@users.noreply.github.com>
2021-01-09 12:07:55 -0800

    fully responsive

--- src/todos.js
+++ src/todos.js
@@ -3,11 +3,16 @@ import React from 'react';
 function Todos  ({todos, deleteTodo, completeTodo })  {
     const todoList = todos.length ?(
         todos.map((todo, index) =>(
+            <React.Fragment>
            <div key={todo.id}  className="collection-item" style={{textDecoration: todo.isCompleted ?("line-through") :("")}}>
-               {todo.content}            
-               <button className="btn-small secondary-content"  onClick = {() => {deleteTodo(todo.id)}}>delete</button>
-               <button className="btn-small secondary-content"  onClick = {() => {completeTodo(index)}}>complete</button> 
-           </div> 
+               <span className="title">{todo.content}</span>
+               <div>
+                    <button className="btn-small"  onClick = {() => {deleteTodo(todo.id)}}>x</button>
+                    <button className="btn-small"  onClick = {() => {completeTodo(index)}}>-</button>
+               </div>  
+            </div> 
+           
+            </React.Fragment>
         ))
     ) :(
         <p className="center">No more todos</p>
